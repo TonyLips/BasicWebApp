0ed811881e40f7c1972865c5dd5cd6c201016448
ThePreparedProcrastinator <anthonyclips18@gmail.com>
2024-03-19 23:01:06 -0400

    Was not able to use Web Scraping, as most sites have disallowed any of that behavior. Found a website which is an api of various different news articles that I will pull from instead. Have made a successful call for 'Video Game News'.

---
+++ BlazorApp-empty1/Models/NewsData.cs
@@ -0,0 +1,28 @@
+﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+public class Article
+{
+    public Source source { get; set; }
+    public string author { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public string url { get; set; }
+    public string urlToImage { get; set; }
+    public DateTime publishedAt { get; set; }
+    public string content { get; set; }
+}
+
+public class NewsData
+{
+    public string status { get; set; }
+    public int totalResults { get; set; }
+    public List<Article> articles { get; set; }
+}
+
+public class Source
+{
+    public string id { get; set; }
+    public string name { get; set; }
+}
+
